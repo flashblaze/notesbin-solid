a74f837d4c8439e37df60ceaba396f5b6c6b4a2d
flashblaze <neerajlagwankar@gmail.com>
2023-05-13 14:43:38 +0530

    Add prisma generate postinstall script

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [
     solid({
-      adapter: vercel(),
+      adapter: vercel({}),
     }),
   ],
   ssr: { external: ["@prisma/client"] },
