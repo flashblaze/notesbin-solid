daad68ee057ed684b38d9fa91352855f726926ee
flashblaze <neerajlagwankar@gmail.com>
2024-11-23 11:59:14 +0530

    Add tailwind and solid ui

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
+
+import "@fontsource/inter";
+import "@fontsource-variable/jetbrains-mono";
 import "./app.css";
 
 export default function App() {
